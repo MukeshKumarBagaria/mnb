bc353ae08fcc1ec0eaddeacdf53e26c1a67e64a9
Mukesh Kumar Bagaria <mukeshkumarbagaria@Mukeshs-MacBook-Air.local>
2026-04-16 13:08:01 +0530

    fixed footer

--- src/app/components/Layout/Footer/index.tsx
+++ src/app/components/Layout/Footer/index.tsx
@@ -32,10 +32,10 @@ const Footer = () => {
   return (
     <footer className='bg-darkmode'>
       <div className='container mx-auto max-w-7xl px-4 sm:px-6 pt-16 md:pt-20 pb-10'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12 gap-10 lg:gap-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-10 lg:gap-8'>
 
           {/* Brand column */}
-          <div className='sm:col-span-2 lg:col-span-4'>
+          <div className='md:col-span-2 lg:col-span-4'>
             <h4 className='text-white text-2xl md:text-3xl font-extrabold mb-3'>
               M&amp;B <span className='text-primary'>Strategy</span>
             </h4>
